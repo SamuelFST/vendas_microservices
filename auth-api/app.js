7e620bd20374497101bcb941e2d4935b91e25baa
SamuelFST <guti.samuel2002@gmail.com>
2023-01-06 20:24:42 -0300

    chore: express.json middleware and auth/user routes added

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,12 +2,19 @@ import dotenv from 'dotenv';
 import express from 'express';
 
 import initialData from './src/config/db/initialData';
+import userRoutes from './src/modules/user/routes/UserRoutes';
+import authRoutes from './src/modules/auth/routes/AuthRoutes';
 
 dotenv.config();
 
 const app = express();
 
 initialData();
+
+app.use(express.json());
+
+app.use(authRoutes);
+app.use(userRoutes);
 
 app.get('/api/status', (req, res) => {
   return res.status(200).json({

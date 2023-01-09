de1741acbbe890c60206d2593959d35744d02736
SamuelFST <guti.samuel2002@gmail.com>
2023-01-09 11:35:53 -0300

    feat: category, Product and Supplier models

---
+++ product-api/src/main/java/com/vendas_microservices/productapi/modules/product/model/Product.java
@@ -0,0 +1,36 @@
+package com.vendas_microservices.productapi.modules.product.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "supplier_id", nullable = false)
+	private Supplier supplier;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
+}
